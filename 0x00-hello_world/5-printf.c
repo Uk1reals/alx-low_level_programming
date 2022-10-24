13c73f1ab5bb8cde39b0a3e4f08f606e51e22734
Uk1reals <royal1demzz@gmail.com>
2022-10-24 10:21:44 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+
+
+
 /**
- * main - Entry Point
  *
- * Return: Always 0 (Success)
- */
+ ** main - Entry point
+ *
+ **
+ *
+ ** Return: Always 0 (success)
+ *
+ **/
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
