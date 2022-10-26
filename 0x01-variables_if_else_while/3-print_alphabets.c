9969b488330f76150bd2e453cbfdb17561eeb1f6
Uk1reals <royal1demzz@gmail.com>
2022-10-26 06:04:45 -0700

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,41 +1,19 @@
-
-Sign up
-Georgeoloche
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x01-variables_if_else_while/3-print_alphabets.c
-@Georgeoloche
-Georgeoloche 3-print_alphabets.c
-…
- 1 contributor
-22 lines (21 sloc)  325 Bytes
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- * Description: prints alphabet in lowercase then uppercase
- * Return: Always 0 (success)
+ * main - main function
+ * Return: Returns 0 if successful
  */
+
 int main(void)
 {
-	int ch;
+	char a;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-		putchar(ch);
-	}
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
 	putchar('\n');
+
 	return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
- * Description: prints numbers
- * Return: Always 0 (success)
+ * main - main function
+ * Return: Returns 0 if successful
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9' ; i++)
+	for (i = 10; i < 20; i++)
 	{
-		putchar(i);
-
-		if (i != '9')
+		putchar((i % 10) + '0');
+		if (i != 19)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
